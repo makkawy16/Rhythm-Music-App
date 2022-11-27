5d35eb4ea8d315770ea2f74ce03a40b803161e84
makkawy16 <yousef.elmakkawy@gmail.com>
2022-11-27 02:34:57 +0200

    initial commit

---
+++ app/src/main/java/com/example/rhythm/ui/authentication/AuthenticationActivity.java
@@ -0,0 +1,22 @@
+package com.example.rhythm.ui.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.rhythm.R;
+import com.example.rhythm.databinding.ActivityAuthenticationBinding;
+
+
+public class AuthenticationActivity extends AppCompatActivity {
+
+    ActivityAuthenticationBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityAuthenticationBinding.inflate(getLayoutInflater());
+        setContentView(R.layout.activity_authentication);
+
+    }
+}
