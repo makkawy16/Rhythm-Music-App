4da88576e48176eaf7f3e5636eb7252ce82aeab6
makkawy16 <yousef.elmakkawy@gmail.com>
2022-12-01 05:44:34 +0200

    finish item singer desgin

---
+++ app/src/main/java/com/example/rhythm/adapter/SingerSuggestAdapter.java
@@ -0,0 +1,18 @@
+package com.example.rhythm.adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class SingerSuggestAdapter {
+
+
+    static class viewHolder extends RecyclerView.ViewHolder{
+
+        public viewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+
+}
