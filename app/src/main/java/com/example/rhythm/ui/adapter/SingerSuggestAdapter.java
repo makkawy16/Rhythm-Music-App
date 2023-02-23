301c0cf733a28c88b312b14d266d81b87aefe775
makkawy16 <yousef.elmakkawy@gmail.com>
2023-02-23 19:02:29 +0200

    desgin

--- app/src/main/java/com/example/rhythm/ui/adapter/SingerSuggestAdapter.java
+++ app/src/main/java/com/example/rhythm/ui/adapter/SingerSuggestAdapter.java
@@ -1,12 +1,29 @@
 package com.example.rhythm.ui.adapter;
 
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class SingerSuggestAdapter {
+public class SingerSuggestAdapter extends RecyclerView.Adapter<SingerSuggestAdapter.viewHolder> {
 
+
+    @NonNull
+    @Override
+    public viewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        return null;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull viewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
 
     static class viewHolder extends RecyclerView.ViewHolder{
 
--- app/src/main/java/com/example/rhythm/ui/authentication/suggestionFragment.java
+++ app/src/main/java/com/example/rhythm/ui/authentication/suggestionFragment.java
@@ -22,35 +22,15 @@ public class suggestionFragment extends Fragment {
     FragmentSuggestionBinding binding;
 
 
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
     public suggestionFragment() {
         // Required empty public constructor
     }
 
 
-    // TODO: Rename and change types and number of parameters
-    public static suggestionFragment newInstance(String param1, String param2) {
-        suggestionFragment fragment = new suggestionFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+
     }
 
     @Override
@@ -63,7 +43,7 @@ public class suggestionFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        binding=FragmentSuggestionBinding.bind(view);
+        binding = FragmentSuggestionBinding.bind(view);
 
         binding.nextBtn.setOnClickListener(new View.OnClickListener() {
             @Override
