a988ab260406c7baeb9a2ef6cbaf3f002465d620
makkawy16 <yousef.elmakkawy@gmail.com>
2023-03-25 07:02:10 +0200

    finish player fragmment design

--- app/src/main/java/com/example/rhythm/ui/homePage/library/SongPlayerFragment.java
+++ app/src/main/java/com/example/rhythm/ui/homePage/library/SongPlayerFragment.java
@@ -1,5 +1,8 @@
 package com.example.rhythm.ui.homePage.library;
 
+import android.graphics.RenderEffect;
+import android.graphics.Shader;
+import android.os.Build;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -11,16 +14,13 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.rhythm.R;
-import com.example.rhythm.databinding.FragmentLikesBinding;
+import com.example.rhythm.databinding.FragmentSongPlayerBinding;
 
+public class SongPlayerFragment extends Fragment {
 
-public class LikesFragment extends Fragment {
+    FragmentSongPlayerBinding binding;
 
-
-    FragmentLikesBinding binding;
-
-
-    public LikesFragment() {
+    public SongPlayerFragment() {
         // Required empty public constructor
     }
 
@@ -28,20 +28,25 @@ public class LikesFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_likes, container, false);
+        return inflater.inflate(R.layout.fragment_song_player, container, false);
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-        binding = FragmentLikesBinding.bind(view);
+        binding = FragmentSongPlayerBinding
+                .bind(view);
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            binding.backgroundImage.setRenderEffect(RenderEffect.createBlurEffect(80,80, Shader.TileMode.MIRROR));
+        }
+
     }
 }
