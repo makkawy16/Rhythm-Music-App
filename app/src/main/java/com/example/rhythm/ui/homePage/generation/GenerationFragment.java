618d75efe0dbb2dae399a111ad9b872d465f28f8
makkawy16 <yousef.elmakkawy@gmail.com>
2023-05-03 22:05:02 +0300

    initial commit in generation

---
+++ app/src/main/java/com/example/rhythm/ui/homePage/generation/GenerationFragment.java
@@ -0,0 +1,48 @@
+package com.example.rhythm.ui.homePage.generation;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.rhythm.R;
+import com.example.rhythm.databinding.FragmentGenerationBinding;
+
+
+public class GenerationFragment extends Fragment {
+
+
+FragmentGenerationBinding binding;
+
+
+    public GenerationFragment() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_generation, container, false);
+
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+binding = FragmentGenerationBinding.bind(view);
+    }
+}
